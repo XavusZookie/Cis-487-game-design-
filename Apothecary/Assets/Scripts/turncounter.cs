ff8d205a70fe3ba97dda42c4839bbbda95868258
XavusZookie <xavuszookie@gmail.com>
2022-10-05 22:23:04 -0400

    combat system(reward)
    added  reward generation after defeat and stop fight at enemy loss

--- Apothecary/Assets/Scripts/turncounter.cs
+++ Apothecary/Assets/Scripts/turncounter.cs
@@ -13,9 +13,11 @@ public class turncounter : MonoBehaviour
     public healthdisplay enemyhealth;
     // Start is called before the first frame update
     bool ourturn = true;
+    bool battlefinished = false;
+    int reward;
     void Start()
     {
-        
+        Random.seed = System.DateTime.Now.Millisecond;
         banner.text = "Your turn!";
     }
 
@@ -24,7 +26,32 @@ public class turncounter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+   
+    }
+
+    public void enemydefeated()
+    {
+        if (enemyhealth.enemyhealthcurrent <= 0)
+        {
+            battlefinished = true;
+            reward = Random.Range(1, 4);
+            switch (reward)
+            {
+                case 1:
+                    banner.text = "Enemy defeated: recieved green herb!";
+                    GameManager.greenherb++;
+                    break;
+                case 2:
+                    banner.text = "Enemy defeated: recieved blue herb!";
+                    GameManager.blueherb++;
+                    break;
+                case 3:
+                    banner.text = "Enemy defeated: recieved red herb!";
+                    GameManager.redherb++;
+                    break;
+
+            }
+        }
     }
 
     public void characterturn()
@@ -38,6 +65,10 @@ public class turncounter : MonoBehaviour
             banner.text = "Opponents turn!";
             enemyhealth.enemyhealthcurrent -= 3;
             ourturn = false;
+            if(enemyhealth.enemyhealthcurrent <=0)
+            {
+                enemydefeated();
+            }
         }
 
 
@@ -45,7 +76,7 @@ public class turncounter : MonoBehaviour
 
     public void opponentturn()
     {
-        if (!ourturn)
+        if (!ourturn && enemyhealth.enemyhealthcurrent > 0)
         {
             
 
@@ -53,34 +84,47 @@ public class turncounter : MonoBehaviour
             GameManager.currenthealth -= 3;
             ourturn=true;
         }
-        
+
+
 
     }
 
     public void slash()
     {
-        if (ourturn)
+        if (ourturn && GameManager.currentstamina >=5)
         {
 
-
+            GameManager.currentstamina -= 5;
             banner.text = "Opponents turn!";
             enemyhealth.enemyhealthcurrent -= Random.Range(1, 3);
             ourturn = false;
+            if (enemyhealth.enemyhealthcurrent <= 0)
+            {
+                enemydefeated();
+            }
         }
+        else if(GameManager.currentstamina <= 5)
+            banner.text = "Not enough stamina";
 
 
     }
 
     public void lunge()
     {
-        if (ourturn)
+        if (ourturn && GameManager.currentstamina >= 3)
         {
 
-
+            GameManager.currentstamina -= 3;
             banner.text = "Opponents turn!";
             enemyhealth.enemyhealthcurrent -= Random.Range(2, 5);
             ourturn = false;
+            if (enemyhealth.enemyhealthcurrent <= 0)
+            {
+                enemydefeated();
+            }
         }
+        else if(GameManager.currentstamina <= 3)
+            banner.text = "Not enough stamina";
 
 
     }
